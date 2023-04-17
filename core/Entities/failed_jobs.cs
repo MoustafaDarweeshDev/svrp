9d1483af549ce6bf11b8f15942bec66fdb2046cb
Karim Elsaeed <karimelsaeedd@gmail.com>
2023-04-17 16:43:56 +0200

    Added Entites

---
+++ core/Entities/failed_jobs.cs
@@ -0,0 +1,15 @@
+﻿namespace core.Entities
+{
+    public class failed_jobs
+    {
+        public int Id { get; set; }
+        public string Uuid { get; set; }
+        public string Connection { get; set; }
+        public string Queue { get; set; }
+        public string Payload { get; set; }
+        public string Exception { get; set; }
+        public DateTime Failed_at { get; set; }
+
+
+    }
+}
---
+++ core/Entities/users.cs
@@ -0,0 +1,22 @@
+﻿namespace core.Entities
+{
+    public class users
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string discriminator { get; set; }
+        public string Email { get; set; }
+        public string Avatar { get; set; }
+        public int Verified { get; set; }
+        public string Locale { get; set; }
+        public string SteamId { get; set; }
+        public string SteamAvatar { get; set; }
+        public string SteamLink { get; set; }
+        public string SteamUsername { get; set; }
+        public int mfa_enabled { get; set; }
+        public string refresh_token { get; set; }
+        public DateTime created_at { get; set; }
+        public DateTime updated_at { get; set; }
+
+    }
+}
