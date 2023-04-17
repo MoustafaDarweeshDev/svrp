f212b9cde850649f163008d84078ad7f544dfd30
MoustafaDarweeshDev <moustafa.darweesh.dev@gmail.com>
2023-04-17 15:43:08 +0200

    added api

---
+++ infrastructure/Data/SvrpContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace infrastructure.Data
+{
+    public class SvrpContext:DbContext
+    {
+        public SvrpContext(DbContextOptions<SvrpContext> options):base(options)
+        {
+
+        }
+
+    }
+}
---
+++ infrastructure/Data/migrations/20230417133938_init.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace infrastructure.Data.migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
